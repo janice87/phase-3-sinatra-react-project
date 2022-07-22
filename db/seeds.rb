2389f030cc5be65eaeaac670094ccfceaf93731e
Janice Chan <janice.chan1288@gmail.com>
2022-07-21 23:10:18 -0400

    seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+# janice = User.create(name: "Janice", email: "janicecodes@gmail.com")
+
+# t1 = Todo.create(todo: "Review for bootcamp")
+# t2 = Todo.create(todo: "30 mins of Yoga")
+# t3 = Todo.create(todo: "Call Lucas's pediatrician")
+
+# m1 = Mood.create(mood: "Stressed", date: "2022-07-21", win: "Took a walk during lunch", challenge: "Busy day at work")
+
+# j1 = Journal.create(date: "2022-07-21", content: "Started working on my project for phase 3, busy day at work and managed to get a walk in." )
 
 puts "✅ Done seeding!"
