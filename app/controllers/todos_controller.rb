701cd84136eafff36d5867bfce4bd9a18caf7b78
Janice Chan <janice.chan1288@gmail.com>
2022-07-23 22:23:31 -0400

    update

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,23 +1,25 @@
+require 'pry'
 class TodosController < ApplicationController
     get '/todos' do
         Todo.all.to_json
     end
 
     get '/todos/:id' do
-        todo = Todo.find(params[:id])
-        todo.to_json
+        @todo = Todo.find(params[:id])
+        @todo.to_json
     end
 
     post '/todos' do
-        todo = Todo.create(
-            todo: params[:todo]
+        @todo = Todo.create(
+            # binding.pry
+           todo: params[:todo]
         )
-        todo.to_json
+        @todo.to_json
     end
 
     delete '/todos/:id' do
-        todo = Todo.find(params[:id])
-        todo.destroy
-        todo.to_json
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        @todo.to_json
     end
 end
